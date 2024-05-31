a77a3191bb4a9dbb9d683e834c88bd0b316f938c
Rashed <rashedulhoqueceo@gmail.com>
2024-05-31 21:36:06 +0600

    create user register route

--- server/index.js
+++ server/index.js
@@ -4,10 +4,17 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 const cors = require("cors");
 
+const authRoutes = require("./routes/auth.js")
+
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
+/* ROUTES */
+app.use("/auth", authRoutes)
+
+
+/* MONGOOSE SETUP */
 const PORT = 3001;
 mongoose
   .connect(process.env.MONGO_URL, {
