657cb33962ec0082347235837897ab8dd82588ae
Rashed <rashedulhoqueceo@gmail.com>
2024-05-31 21:25:23 +0600

    create user schima

--- server/index.js
+++ server/index.js
@@ -7,3 +7,15 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
+
+const PORT = 3001;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    dbName: "Dream_Nest",
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  })
+  .catch((err) => console.log(`${err} did not connect`));
