bfc8a1e6a433fa961291eb29c7ac2d5efad9285d
Rashed <rashedulhoqueceo@gmail.com>
2024-05-31 20:29:16 +0600

    make register page input filed

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -2,7 +2,55 @@ import React from 'react'
 
 const RegisterPage = () => {
   return (
-    <div>RegisterPage</div>
+    <div className="register">
+    <div className="register_content">
+      <form className="register_content_form">
+        <input
+          placeholder="First Name"
+          name="firstName"
+          required
+        />
+        <input
+          placeholder="Last Name"
+          name="lastName"
+          required
+        />
+        <input
+          placeholder="Email"
+          name="email"
+          type="email"
+          required
+        />
+        <input
+          placeholder="Password"
+          name="password"
+          type="password"
+          required
+        />
+        <input
+          placeholder="Confirm Password"
+          name="confirmPassword"
+          type="password"
+          required
+        />
+        <input
+          id="image"
+          type="file"
+          name="profileImage"
+          accept="image/*"
+          style={{ display: "none" }}
+          required
+        />
+        <label htmlFor="image">
+          <img src="/assets/addImage.png" alt="add profile photos" />
+          <p>Upload Your Photo</p>
+        </label>
+
+        <button type="submit">REGISTER</button>
+      </form>
+      <a href="/login">Already have an account? Log In Here</a>
+    </div>
+  </div>
   )
 }
 
